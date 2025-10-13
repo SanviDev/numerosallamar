4821f93e75ba1e074b09fc49cdaac8e83708351f
SanviDev <smcaldabe@gmail.com>
2025-10-13 12:28:34 -0300

    functions

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import db from "./config/db.js";
 
 const app = express();
 
+// Middleware para parsear JSON y datos de formularios HTML
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 // Necesario para obtener __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,10 +17,19 @@ const __dirname = path.dirname(__filename);
 // Servir la carpeta "public" como estática
 app.use(express.static(path.join(__dirname, "public")));
 
+
+// Rutas frontend
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/clientes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "html", "clientes.html"));
+});
+
+// entradas de la api
+
+// GET /api/numeros - Obtener todos los números
 app.get("/api/numeros", (req, res) => {
   db.query("SELECT * FROM numeros", (err, results) => {
     if (err) {
@@ -27,18 +41,20 @@ app.get("/api/numeros", (req, res) => {
   });
 });
 
+// POST /api/numeros - Agregar un nuevo número
 app.post("/api/numeros", (req, res) => {
-  const { name, phone, social_network, category } = req.body;
-  db.query("INSERT INTO numeros (nombre, numero, red_social, categoria) VALUES (?, ?, ?, ?)", [name, phone, social_network, category], (err, results) => {
+  const { nombre, numero, red_social, categoria } = req.body;
+  db.query("INSERT INTO numeros (nombre, numero, red_social, categoria) VALUES (?, ?, ?, ?)", [nombre, numero, red_social, categoria], (err, results) => {
     if (err) {
       console.error("Error adding contact:", err);
       res.status(500).json({ error: "Internal Server Error" });
       return;
     }
-    res.status(201).json({ id: results.insertId, nombre: name, numero: phone, red_social: social_network, categoria: category });
+    res.status(201).json({ id: results.insertId, nombre: nombre, numero: numero, red_social: red_social, categoria: categoria });
   });
 });
 
+// GET /api/clientes - Obtener todos los clientes
 app.get("/api/clientes", (req, res) => {
   db.query("SELECT * FROM clientes", (err, results) => {
     if (err) {
@@ -50,18 +66,22 @@ app.get("/api/clientes", (req, res) => {
   });
 });
 
+// POST /api/clientes - Agregar un nuevo cliente
 app.post("/api/clientes", (req, res) => {
-  const { numberId, tipoWeb, fechaLimite } = req.body;
-  db.query("INSERT INTO clientes (numberId, tipoWeb, fechaLimite) VALUES (?, ?, ?)", [numberId, tipoWeb, fechaLimite], (err, results) => {
+  const { numberId, descripcion, fechaLimite } = req.body;
+  db.query("INSERT INTO clientes (numberId, descripcion, fechaLimite) VALUES (?, ?, ?)", [numberId, descripcion, fechaLimite], (err, results) => {
     if (err) {
       console.error("Error adding client:", err);
       res.status(500).json({ error: "Internal Server Error" });
       return;
     }
-    res.status(201).json({ id: results.insertId, numberId, tipoWeb, fechaLimite });
+    res.status(201).json({ id: results.insertId, numberId, descripcion, fechaLimite });
   });
 });
 
+
+
+// arrancar el servidor
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,23 +1,92 @@
 const main = document.querySelector('main');
 
-fetch('/api/numeros')
-    .then(response => response.json())
-    .then(data => {
-        if (data.length == 0) {
-            main.innerHTML = '<p class="no-results">No hay numeros pendientes.</p>';
-            return;
-        }
-        data.forEach(contact => {
-            const contactDiv = document.createElement('div');
-            contactDiv.classList.add('contact');
-            contactDiv.innerHTML = `
-                <h2>${contact.name}</h2>
-                <p>Phone: ${contact.phone}</p>
-                <p>Email: ${contact.email}</p>
+
+function renderList() {
+    fetch('/api/numeros')
+        .then(response => response.json())
+        .then(data => {
+            main.innerHTML = '';
+            if (data.length == 0) {
+                main.innerHTML = '<p class="no-results">No hay numeros pendientes.</p>';
+                return;
+            }
+            data.forEach(contact => {
+                const contactDiv = document.createElement('div');
+                contactDiv.classList.add('contact');
+                contactDiv.innerHTML = `
+                <h2>${contact.nombre}</h2>
+                <p>Teléfono: ${contact.numero}</p>
+                <p>Red Social: ${contact.red_social}</p>
+                <p>Categoría: ${contact.categoria}</p>
+                <div class="buttons-contact">
+                    <button class="delete" data-id="delete-${contact.id}">Eliminar</button>
+                    <button class="definir" data-id="def-${contact.id}">Mover a Clientes</button>
+                </div>
             `;
-            main.appendChild(contactDiv);
+                main.appendChild(contactDiv);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching contacts:', error);
         });
-    })
-    .catch(error => {
-        console.error('Error fetching contacts:', error);
+
+    setTimeout(() => { renderList() }, 1000);
+}
+
+renderList();
+
+
+// Mostrar el formulario al hacer clic en el botón "Agregar número"
+document.getElementById('addNumber').addEventListener('click', () => { 
+    document.querySelector('.add-numero').style.display = 'block';
+});
+
+
+
+
+
+
+// Manejar el envío del formulario para agregar un nuevo número
+document.getElementById("numberForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = {
+        nombre: e.target.nombre.value,
+        numero: e.target.numero.value,
+        red_social: e.target["red-social"].value,
+        categoria: e.target.categoria.value,
+    };
+
+    const res = await fetch("/api/numeros", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
     });
+
+    if (res.ok) {
+        document.querySelector('.add-numero').style.display = 'none';
+        alert("Número agregado correctamente");
+        e.target.reset();
+    } else {
+        alert("<p style='color: red;'>Error al agregar el número</p>");
+        const errorData = await res.json();
+        console.error("Error details:", errorData);
+    }
+});
+
+
+// Manejar el clic en el botón "Cancelar" para ocultar el formulario
+document.getElementById("cancelAdd").addEventListener("click", () => {
+    document.querySelector('.add-numero').style.display = 'none';
+});
+
+
+document.querySelectorAll('.definir').forEach(element => {
+    element.addEventListener('click', async (e) => {
+        const id = e.target.getAttribute('data-id').split('-')[1];
+        // Aquí puedes agregar la lógica para mover el número a "Clientes"
+        // Por ejemplo, hacer una solicitud POST a /api/clientes con el ID del número
+        console.log('Mover a Clientes ID:', id);
+        
+    });
+});
